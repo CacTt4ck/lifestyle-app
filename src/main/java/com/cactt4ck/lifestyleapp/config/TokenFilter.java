95c0e0a68e768f82616b0a638096147975ae21af
Thomas C <lachainedelightning13090@gmail.com>
2024-10-06 15:02:30 +0200

    Add image support

---
+++ src/main/java/com/cactt4ck/lifestyleapp/config/TokenFilter.java
@@ -0,0 +1,62 @@
+package com.cactt4ck.lifestyleapp.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Slf4j
+@Component
+public class TokenFilter extends OncePerRequestFilter {
+
+    @Value("${lifestyle-app.token}")
+    private String bearerToken;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader("Authorization").substring(7);
+        log.info("Token: {}", bearerToken);
+
+        String encryptedBearerToken = this.encryptToken(bearerToken);
+        String encryptedToken = this.encryptToken(token);
+
+        if (encryptedToken == null || !encryptedToken.equals(encryptedBearerToken)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Unauthorized");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String encryptToken(String token) {
+        String encryptedToken = null;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            byte[] hashBytes = digest.digest(token.getBytes());
+
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashBytes) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            encryptedToken = hexString.toString().toUpperCase();
+        } catch (NoSuchAlgorithmException e) {
+            log.error(e.getMessage());
+        }
+        return encryptedToken;
+    }
+
+}
--- src/main/java/com/cactt4ck/lifestyleapp/repository/RecipeRepository.java
+++ src/main/java/com/cactt4ck/lifestyleapp/repository/RecipeRepository.java
@@ -1,7 +1,17 @@
 package com.cactt4ck.lifestyleapp.repository;
 
+import com.cactt4ck.lifestyleapp.model.Image;
 import com.cactt4ck.lifestyleapp.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+
+    @Modifying
+    @Query("update Recipe r set r.image = :uri where r.id = :id")
+    void addImage(@Param("id") Long recipeId, @Param("uri") String imageURI);
 }
